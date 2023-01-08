81d6738f00ced1702ce4d046401bc21e463e3ab5
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2023-01-07 23:37:57 -0300

    Creating an ALU

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
 
+int notGate(int A) {
+
+    if (A == 1) {
+        return 0;
+    }
+
+    return 1;
+
+}
+
 int andGate(int A, int B) {
 
     if ((A && B) == 1) {
@@ -51,9 +61,7 @@ void adder(int A, int B, int C, int output[2]){
 
 }
 
-void fourBitAdder(int firstFourBitsInput[4], int secondFourBitsInput[4], int carry){
-
-    int output[5];
+void fourBitAdder(int firstFourBitsInput[4], int secondFourBitsInput[4], int carry, int output[5]){
 
     int aux[2];
 
@@ -68,16 +76,43 @@ void fourBitAdder(int firstFourBitsInput[4], int secondFourBitsInput[4], int car
 
     output[4] = aux[1];
 
-    printf("The sum is %d %d %d %d , and the carry is %d", output[0], output[1], output[2], output[3], output[4]);
+    //printf("The sum is %d %d %d %d , and the carry is %d", output[0], output[1], output[2], output[3], output[4]);
+
+}
+
+void ArithmeticLogicUnit(int firstFourBitsInput[4], int secondFourBitsInput[4], int operation){
+
+    int outputAdder[5];
+
+    int treatedSecondFourBitsInput[4];
+
+    for(int i = 0; i < 4; i++){
+        treatedSecondFourBitsInput[i] = exclusiveOr(secondFourBitsInput[i],operation);
+    }
+
+    fourBitAdder(firstFourBitsInput,treatedSecondFourBitsInput,operation,outputAdder);
+
+    int output[7];
+
+    output[0] = outputAdder[0]; // Number
+    output[1] = outputAdder[1]; // Number
+    output[2] = outputAdder[2]; // Number
+    output[3] = outputAdder[3]; // Number
+
+    output[4] = outputAdder[4]; // Carry
+    output[5] = outputAdder[0]; // Is it negative ?
+    output[6] = andGate(andGate(andGate(notGate(output[0]),notGate(output[1])), notGate(output[2])), notGate(output[3])); // Zero
+
+    printf(" THE SUM IS %d %d %d %d \n THE CARRY IS %d \n IS IT NEGATIVE ? %d \n IS IT ZERO ? %d\n", output[0],output[1],output[2],output[3],output[4],output[5],output[6]);
 
 }
 
 int main() {
 
-    int firstFourBitsInput[4] = {0,0,1,1};
-    int secondFourBitsInput[4] = {0,0,0,1};
+    int firstFourBitsInput[4] = {1,1,1,0};
+    int secondFourBitsInput[4] = {1,1,0,0};
 
-    fourBitAdder(firstFourBitsInput,secondFourBitsInput,0);
+    ArithmeticLogicUnit(firstFourBitsInput,secondFourBitsInput, 0);
 
     return 0;
 
